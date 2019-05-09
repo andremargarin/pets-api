375690c5afbbad509c53f1e20e0f968d08310468
André Augusto Margarin <andre.margarin@gmail.com>
2019-05-09 12:08:30 -0300

    Remove unnecessary urls

--- pets_api/urls.py
+++ pets_api/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/v1/auth/', include('rest_framework.urls')),
     path('api/v1/', include('pets.urls'))
 ]
