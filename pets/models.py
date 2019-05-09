99bfe819fce4a37ccea223f8b675114fe9c45f66
André Augusto Margarin <andre.margarin@gmail.com>
2019-05-09 11:01:53 -0300

    Create pet and person model

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=200)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
